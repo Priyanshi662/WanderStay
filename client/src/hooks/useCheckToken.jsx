e69065ac22316daf52e92a3e3fc24738d9695183
Priyanshi662 <priyanshiw567@gmail.com>
2023-06-26 15:05:59 +0530

    useCheckToken to be used at client side

--- client/src/hooks/useCheckToken.jsx
+++ client/src/hooks/useCheckToken.jsx
@@ -1,5 +1,5 @@
 import {useEffect} from 'react'
-import { useValue } from '../../client/src/context/ContextProvider';
+import { useValue } from '../context/ContextProvider';
 import jwtDecode from 'jwt-decode';
 const useCheckToken=()=>{
     const {state:{currentUser},dispatch}=useValue();
